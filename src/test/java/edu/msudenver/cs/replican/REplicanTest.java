02992d1f8b3f6c57d74915aa8ad07cdf2c7985cc
Jvitatoe <Vitatoe5929@msn.com>
2018-04-05 09:25:49 -0600

    mock up start

--- src/test/java/edu/msudenver/cs/replican/REplicanTest.java
+++ src/test/java/edu/msudenver/cs/replican/REplicanTest.java
@@ -1,14 +1,23 @@
 package edu.msudenver.cs.replican;
 
+import static org.mockito.Mockito.when;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.InjectMocks;
 
+@RunWith(MockitoJUnitRunner.class)
 public class REplicanTest {
 	
 	private final REplican replican = new REplican();
+    @Mock
+    private Cookies cookies;
 
-	@Test
+    @Test
 	public void testEscapeURL() throws Exception {
 		assertEquals (P38.call("escapeURL", replican, new Object[]{"foo"}),	"foo");
 		assertEquals (P38.call("escapeURL", replican, new Object[]{"^foo"}), "\\^foo");
